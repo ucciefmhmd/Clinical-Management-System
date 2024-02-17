8a74b096e21275742e9bb860bbefdfb05ab4791e
Youssef Moahmed <155498111+ucciefmhmd@users.noreply.github.com>
2024-02-17 17:56:41 +0200

    Add files via upload

---
+++ ClinicDB.cs
@@ -0,0 +1,27 @@
+﻿using C_sharp_Project.YoussifMohamed.Model.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_Project.YoussifMohamed.Model.Database
+{
+    public class ClinicDB : DbContext
+    {
+        public DbSet<Patient> Patient { get; set; }
+        public DbSet<Doctor> Doctor { get; set; }
+        public DbSet<Reciptionist> Reciptionist { get; set; }
+        public DbSet<Doctorspatient> Doctorspatient { get; set; }
+        public DbSet<ReciptionAddPatientDoc> ReciptionAddPatientDoc { get; set; }
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-7DE5S9C\\SQLEXPRESS;Database=DBClinicalManagement;Encrypt=false;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+ 
+    }
+
+}
